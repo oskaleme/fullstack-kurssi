eb178951f64d9c9c8ddd73e2b70f423edfe1772d
Oskar Mella <oskarmella@Oskar-MacBook-Air.local>
2025-05-05 18:50:37 +0300

    Tehtävät 1.1 ja  1.2

---
+++ osa1/part1/src/App.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+// 0) Part-komponentti
+const Part = (props) => (
+  <p>
+    {props.name} {props.exercises}
+  </p>
+)
+
+// 1) Header-komponentti
+const Header = (props) => <h1>{props.course}</h1>
+
+// 2) Content-komponentti käyttää part
+const Content = (props) => (
+  <div>
+    <Part name={props.part1} exercises={props.exercises1} />
+    <Part name={props.part2} exercises={props.exercises2} />
+    <Part name={props.part3} exercises={props.exercises3} />
+  </div>
+)
+
+// 3) Total-komponentti
+const Total = (props) => (
+  <p>
+    Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+  </p>
+)
+
+// 4) App-komponentti
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
+  )
+}
+
+export default App
