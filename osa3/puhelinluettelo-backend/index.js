35a20aa2684d493a9499dabb6dca46986a230d3b
Oskar Mella <oskarmella@Oskar-MacBook-Air.local>
2026-02-01 13:56:46 +0200

    osa3: 3.11

--- osa3/puhelinluettelo-backend/index.js
+++ osa3/puhelinluettelo-backend/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
-
+app.use(express.static('dist'))
 //3.8
 morgan.token('body', (req) => {
   return (['POST','PUT','PATCH'].includes(req.method) && req.body && Object.keys(req.body).length)
