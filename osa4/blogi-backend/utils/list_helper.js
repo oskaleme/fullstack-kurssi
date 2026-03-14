c9f8723e0184e9aa9c569f3a202c433357d2dad9
Oskar Mella <oskarmella@Oskar-MacBook-Air.local>
2026-03-14 13:50:44 +0200

    osa4: 4.3-4.5

---
+++ osa4/blogi-backend/utils/list_helper.js
@@ -0,0 +1,22 @@
+const dummy = (blogs) => {
+    return 1
+  }
+  
+  const totalLikes = (blogs) => {
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+  }
+  
+  const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+      return null
+    }
+  
+    return blogs.reduce((fav, blog) => {
+      return blog.likes > fav.likes ? blog : fav
+    })
+  }
+  module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog
+  }
