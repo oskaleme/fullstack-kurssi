53bc28e7047ac6f4ba4e0a9ed6b406c2ddf2cdb7
Oskar Mella <oskarmella@Oskar-MacBook-Air.local>
2026-03-26 19:36:29 +0200

    osa4: 4.6-4.7

--- osa4/blogi-backend/utils/list_helper.js
+++ osa4/blogi-backend/utils/list_helper.js
@@ -15,8 +15,65 @@ const dummy = (blogs) => {
       return blog.likes > fav.likes ? blog : fav
     })
   }
+
+  const mostBlogs = (blogs) => {
+    if (blogs.length === 0) {
+      return null
+    }
+  
+    const counts = {}
+  
+    blogs.forEach(blog => {
+      counts[blog.author] = (counts[blog.author] || 0) + 1
+    })
+  
+    let topAuthor = null
+    let topBlogs = 0
+  
+    Object.keys(counts).forEach(author => {
+      if (counts[author] > topBlogs) {
+        topAuthor = author
+        topBlogs = counts[author]
+      }
+    })
+  
+    return {
+      author: topAuthor,
+      blogs: topBlogs
+    }
+  }
+  
+  const mostLikes = (blogs) => {
+    if (blogs.length === 0) {
+      return null
+    }
+  
+    const likes = {}
+  
+    blogs.forEach(blog => {
+      likes[blog.author] = (likes[blog.author] || 0) + blog.likes
+    })
+  
+    let topAuthor = null
+    let topLikes = 0
+  
+    Object.keys(likes).forEach(author => {
+      if (likes[author] > topLikes) {
+        topAuthor = author
+        topLikes = likes[author]
+      }
+    })
+  
+    return {
+      author: topAuthor,
+      likes: topLikes
+    }
+  }
+
   module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
   }
