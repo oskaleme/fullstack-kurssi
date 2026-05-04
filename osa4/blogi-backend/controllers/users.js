71e09f0934cca5d85a0ca6145125da9b22cb4ee5
Oskar Mella <oskarmella@Oskar-MacBook-Air.local>
2026-05-04 19:08:34 +0300

    osa4: 4.15

---
+++ osa4/blogi-backend/controllers/users.js
@@ -0,0 +1,34 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+})
+
+usersRouter.post('/', async (request, response, next) => {
+  try {
+    const { username, name, password } = request.body
+
+    if (!password) {
+      return response.status(400).json({ error: 'password missing' })
+    }
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    })
+
+    const savedUser = await user.save()
+    response.status(201).json(savedUser)
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = usersRouter
